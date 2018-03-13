e95d12131948d996ac869b0ae40bd3e67cc5e9f0
HoLLy <dario.db@hotmail.com>
2018-03-13 16:07:29 +0100

    Create Hex project

---
+++ osu.Game.Rulesets.HoLLy.Hex/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("osu!Hex - A osu!lazer ruleset")]
+[assembly: AssemblyDescription("The osu!Hex ruleset")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HoLLy")]
+[assembly: AssemblyProduct("osu!Hex")]
+[assembly: AssemblyCopyright("Copyright © HoLLy 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f1345495-8237-4dc4-add3-5f48855c81b9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
